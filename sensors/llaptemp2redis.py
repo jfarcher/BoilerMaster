1a71a412fb06b532abe437daf6360adf490fd569
jfarcher <jf_archer@yahoo.com>
2014-11-13 21:05:44 +0000

    Inclusion of mqtt for use in web interface

--- sensors/llaptemp2redis.py
+++ sensors/llaptemp2redis.py
@@ -8,6 +8,7 @@ import re
 import sys
 import signal
 from ConfigParser import SafeConfigParser
+import paho.mqtt.client as mqtt
 
 def signal_handler(signal, frame):
     sys.exit(0)
@@ -18,17 +19,21 @@ parser = SafeConfigParser()
 parser.read('/etc/boilermaster/config.ini')  
 
 rbroker = parser.get('redis', 'broker')
+mbroker = parser.get('mqtt', 'broker')
 rtcpport = parser.get('redis', 'port')
+mtcpport = parser.get('mqtt', 'port')
 baud = 115200
 port = '/dev/ttyAMA0'
 ser = serial.Serial(port, baud)
 topic = parser.get('temps', 'topic')
-
+mqttc = mqtt.Client()
 
 mypid = os.getpid()
 
 #Connect to broker
-redthis = redis.StrictRedis(host=rbroker,port=rtcpport, db=0)                                                                                                                                                      
+redthis = redis.StrictRedis(host=rbroker,port=rtcpport, db=0)
+mqttc.connect (mbroker, mtcpport, 60)
+                                                                                                                   
 
 while True:
 	llapMsg = ser.read(12)
@@ -39,3 +44,4 @@ while True:
 			temp = temp[:-1]
 	if sensor == "TEMP":
 		redthis.set(topic + devID + "/sensor",temp) 
+		mqttc.publish(topic + devID + "/sensor",temp, retain=True)
