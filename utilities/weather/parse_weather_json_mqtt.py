fc7c0e76f5c3a9e0ebcdae12bcd37a4ac7af1f33
jfarcher <jf_archer@yahoo.com>
2014-12-04 00:24:28 +0000

    changes for rename of config file

--- utilities/weather/parse_weather_json_mqtt.py
+++ utilities/weather/parse_weather_json_mqtt.py
@@ -17,7 +17,7 @@ def on_exit(sig, func=None):
     sys.exit(1)
 
 parser = SafeConfigParser()
-parser.read('/etc/boilermaster/config.ini')
+parser.read('/etc/boilermaster/boilermaster.conf')
 broker = parser.get('mqtt', 'broker')
 tcpport = parser.get('mqtt', 'port')
 url = parser.get('weather', 'url')
